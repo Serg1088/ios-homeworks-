7659070d94b10f588a56c3713554292661ad0d1f
Сергей Белкин <seryoga88@icloud.com>
2022-05-27 22:02:42 +0300

    Добавлен проект по ДЗ 1.2

---
+++ Netology_IB_Instruments/Netology_IB_Instruments/ProfileView.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileView.swift
+//  Netology_IB_Instruments
+//
+//  Created by Сергей Белкин on 22.05.2022.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+
+    @IBOutlet weak var imageWaiter: UIImageView!
+    @IBOutlet weak var labelName: UILabel!
+    @IBOutlet weak var labelYear: UILabel!
+    @IBOutlet weak var labelCity: UILabel!
+    @IBOutlet weak var textView: UITextView!
+}
