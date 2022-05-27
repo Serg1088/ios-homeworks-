e1f97eafffd64050b87f661f2d973f7e4b06e534
Сергей Белкин <seryoga88@icloud.com>
2022-05-27 21:33:34 +0300

    Initial Commit

---
+++ Shared/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Shared
+//
+//  Created by Сергей Белкин on 27.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
