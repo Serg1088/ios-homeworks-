5b37f2ef9dc5b58a5faaed0a7c9fc51cff92d537
Sergei Belkin <103189906+Serg1088@users.noreply.github.com>
2022-06-03 13:17:05 +0300

    Добавлен проект ДЗ 1.3
    Навигация в iOS и жизненный цикл UIViewController

---
+++ Navigation/Navigation/ProfileViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileViewController.swift
+//  Navigation
+//
+//  Created by Сергей Белкин on 01.06.2022.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemMint
+        self.navigationItem.title = "ПРОФИЛЬ"
+    }
+
+}
