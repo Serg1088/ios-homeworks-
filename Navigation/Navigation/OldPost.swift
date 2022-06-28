08e9190e3b28f995bef8713a25437e7e59119fde
Sergei Belkin <103189906+Serg1088@users.noreply.github.com>
2022-06-28 21:38:37 +0300

    Обновленный проект по ДЗ 2.3(ч.2)
    Обновленный проект по ДЗ 2.3(ч.2)

---
+++ Navigation/Navigation/OldPost.swift
@@ -0,0 +1,11 @@
+//
+//  OldPost.swift
+//  Navigation
+//
+//  Created by Сергей Белкин on 27.06.2022.
+//
+
+import Foundation
+struct Post {
+    var title :String
+}
---
+++ Navigation/Navigation/Profile/Post.swift
@@ -0,0 +1,21 @@
+//
+//  Post.swift
+//  Navigation
+//
+//  Created by Сергей Белкин on 25.06.2022.
+//
+
+import Foundation
+import UIKit
+
+struct PostStruct {
+    
+    var author: String
+    var description: String
+    var image: String
+    var likes: Int
+    var views: Int
+
+}
+
+
